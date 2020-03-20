00d763a6c5e5fbb73c2e088261611f64bdcd8943
zlicina1 <zlicina1@etf.unsa.ba>
2020-03-20 11:16:45 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TextField n;
+    
+    public TextArea ispis;
+}
